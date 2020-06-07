2de12dc5586be493a20ba950372f4e6cd55d6fcc
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-06-07 16:56:51 +0200

    Add actions for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
+  before_action authenticate_member!, except: [:index]
   def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :members
 
-  resources :posts
+  resources :posts, only: [:index, :new, :create]
 
   root to: 'posts#index'
 end
