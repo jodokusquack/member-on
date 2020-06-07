23d369f2e7dc6e062440d62746b3e6e7848b9c98
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-06-07 15:37:10 +0200

    Add devise, bulma and guard-livereload and install them

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Devies Mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Rack-livereload
+  config.middleware.insert_before ActionDispatch::Static, Rack::LiveReload
 end
